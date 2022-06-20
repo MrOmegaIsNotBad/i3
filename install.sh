15b7449a3cf34774f53427efb8338f8136374093
Ваше Имя <you@example.com>
2022-06-20 19:15:39 +0300

    v

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ uk_UA.UTF-8 UTF-8
 sudo locale-gen
 sudo localectl set-locale ru_RU.UTF-8
 
-sudo pacman -Sy viewnior zsh imagemagick wget rander xautolock polkit-gnome git cmus gufw gparted blueman neovim vim qt5ct htop flameshot gimp telegram-desktop firefox discord noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji kitty xterm xfce4-terminal thunar pcmanfm lxappearance xfce4-power-manager pulseaudio pavucontrol redshift xfce4-clipman-plugin mpv tmux gvim
+sudo pacman -Sy viewnior zsh imagemagick wget xclip ranger rxvt-unicode unicode-emoji unicode-cldr xautolock polkit-gnome git cmus gufw gparted blueman neovim vim qt5ct htop flameshot gimp telegram-desktop firefox discord noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji kitty pcmanfm lxappearance xfce4-power-manager pulseaudio pavucontrol redshift xfce4-clipman-plugin mpv tmux gvim
 
 sudo systemctl enable bluetooth
 
